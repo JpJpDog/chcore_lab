0383350fad706ad17b9afa8e9e6ab15cdcd4319b
shenyushi <shenyushi@sjtu.edu.cn>
2021-03-10 00:58:05 +0800

    finish code part

--- kernel/monitor.c
+++ kernel/monitor.c
@@ -17,9 +17,9 @@
 #include <common/types.h>
 
 static inline __attribute__ ((always_inline))
-u64 read_fp()
+u64 *read_fp()
 {
-	u64 fp;
+	u64 *fp;
 	__asm __volatile("mov %0, x29":"=r"(fp));
 	return fp;
 }
@@ -30,6 +30,23 @@ int stack_backtrace()
 	printk("Stack backtrace:\n");
 
 	// Your code here.
+
+	u64 *this_fp = read_fp(), *prev_fp, *lr;
+	u64 args[5];
+
+	while ((prev_fp = (u64*) *this_fp) != 0) {
+		lr = (u64 *)*(prev_fp + 1);
+		int args_i = 0;
+		for (u64* p= this_fp + 2; p != prev_fp && args_i <5; p++) {
+			args[args_i++] = *p;
+		}
+		printk("LR %lx FP %lx Args ", lr, prev_fp);
+		for (int i = 0; i < args_i; i++) {
+			printk("%lx ", args[i]);
+		}
+		printk("\n");
+		this_fp = prev_fp;
+	}
 
 	return 0;
 }
